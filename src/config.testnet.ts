2ee04220601826e61858b233023dc84c67de9945
Lonnie Moy <lonnie@allstar.io>
2023-04-16 17:12:14 -0600

    contract update

--- src/config.testnet.ts
+++ src/config.testnet.ts
@@ -55,7 +55,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqnlllllsja3cq7'
 };
 
 export const delegationContractData: Array<DelegationContractType> = [
